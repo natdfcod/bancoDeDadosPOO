fd5ca719a3f697db83d5406b9d6ef3731b3173da
Nicolas <nick10052007@gmail.com>
2026-08-13 22:29:29 -0300

    voltando versao

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,11 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         //teste inserindo categoria
-        // CategoriaDAO categoriaDAO = new CategoriaDAO();
-        // Categoria categoria = new Categoria();
-        // categoria.setCategoria("abc");
+        CategoriaDAO categoriaDAO = new CategoriaDAO();
+        Categoria categoria = new Categoria();
+        categoria.setCategoria("abc");
 
-        // categoriaDAO.inserir(categoria);
+        categoriaDAO.inserir(categoria);
 
         //teste listagem categoria
         List<Categoria> lista = categoriaDAO.listar();
@@ -16,18 +16,18 @@ public class Main {
             System.out.printf("|ID - %d| |Categoria - %s|\n", c.getId(), c.getCategoria());
         }
 
-        // //testar despesa
-        // //inserir
-        // DespesaDAO despesaDAO = new DespesaDAO();
-        // Despesa despesa = new Despesa();
+        //testar despesa
+        //inserir
+        DespesaDAO despesaDAO = new DespesaDAO();
+        Despesa despesa = new Despesa();
 
-        // despesa.setDescricao("Kenny tonto");
-        // despesa.setValor(1.50);
-        // despesa.setData(LocalDate.now());
-        // Categoria c = new Categoria();
-        // c.setId(41);
-        // despesa.setCategoria(c);
-        // despesaDAO.inserir(despesa);
+        despesa.setDescricao("Kenny tonto");
+        despesa.setValor(1.50);
+        despesa.setData(LocalDate.now());
+        Categoria c = new Categoria();
+        c.setId(41);
+        despesa.setCategoria(c);
+        despesaDAO.inserir(despesa);
 
 
 
