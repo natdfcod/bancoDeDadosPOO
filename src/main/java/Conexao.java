76aca56ac7c4240c65a7e1b4c8541af054d57287
natdfcod <nick10052007@gmail.com>
2026-08-18 10:58:34 -0300

    ajustes

--- src/main/java/Conexao.java
+++ src/main/java/Conexao.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class Conexao {
     private final String url = "jdbc:oracle:thin:@oracle.fiap.com.br:1521:ORCL";
-    private final String user = "rm561579";
-    private final String password = "100507";
+    private final String user = "";
+    private final String password = "";
 
     public Connection conectar() {
         try{
