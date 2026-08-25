dde9a0b838fae37adb8bdc1a59d1ddaa8d787a93
Laboratório FIAP <nick10052007@gmail.com>
2026-08-25 11:39:57 -0300

    final de aula

--- src/main/java/DAO/VendaDAO.java
+++ src/main/java/DAO/VendaDAO.java
@@ -4,10 +4,7 @@ import Factory.ConnectionFactory;
 import Model.Venda;
 import Model.Vendedor;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +18,7 @@ public class VendaDAO implements GenericDAO<Venda, Integer> {
         PreparedStatement ps = connection.prepareStatement(sql)){
             ps.setInt(1, entidade.getVendedor().getId());
             ps.setDouble(2, entidade.getTotal());
-            ps.setDate(3, entidade.getData());
+            ps.setDate(3, Date.valueOf(entidade.getData()));
             ps.execute();
         } catch (SQLException e) {
             System.out.println("Erro ao inserir Vendedor: " + e.getMessage());
@@ -29,21 +26,27 @@ public class VendaDAO implements GenericDAO<Venda, Integer> {
     }
 
     @Override
-    public List<Vendedor> listar() {
-        String sql = "select * from java_vendedor";
-        List<Vendedor> listaVendedor = new ArrayList<>();
+    public List<Venda> listar() {
+        String sql = "select * from java_venda";
+        List<Venda> listaVenda = new ArrayList<>();
         try(Connection connection = ConnectionFactory.obterConexao();
             PreparedStatement ps = connection.prepareStatement(sql)){
             ResultSet rs = ps.executeQuery();
             while (rs.next()){
+                Venda venda = new Venda();
                 Vendedor vendedor = new Vendedor();
-                vendedor.setId(rs.getInt("id"));
-                vendedor.setNome(rs.getString("nome"));
-                listaVendedor.add(vendedor);
+
+                venda.setId(rs.getInt("id"));
+                vendedor.setId(rs.getInt("id_vendedor"));
+                venda.setTotal(rs.getDouble("total"));
+                venda.setData(rs.getDate("data").toLocalDate());
+
+                venda.setVendedor(vendedor);
+                listaVenda.add(venda);
             }
         } catch (SQLException e) {
             System.out.println("Erro ao listar Vendedor" + e.getMessage());
         }
-        return  listaVendedor;
+        return listaVenda;
     }
 }
--- src/main/java/view/MenuVenda.java
+++ src/main/java/view/MenuVenda.java
@@ -1,12 +1,16 @@
 package view;
 
+import DAO.VendaDAO;
 import DAO.VendedorDAO;
+import Model.Venda;
 import Model.Vendedor;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
-import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
-import static javax.swing.JOptionPane.showInputDialog;
+import static java.lang.Double.parseDouble;
+import static javax.swing.JOptionPane.*;
 
 public class MenuVenda {
     public void menu(){
@@ -26,13 +30,27 @@ public class MenuVenda {
     }
 
     private void listar() {
+        List<Venda> lista = new VendaDAO().listar();
+        String aux = "";
+        for (Venda venda : lista){
+            aux += "|id =  "+ venda.getId()+" | id_vendedor = "+ venda.getVendedor().getId()+" | total = "+venda.getTotal()+" | data = "+venda.getData()+"|\n";
+        }
+        showMessageDialog(null, aux);
     }
 
     private void inserir() {
         Vendedor vendedor = new Vendedor();
         List<Vendedor> lista = new VendedorDAO().listar();
+        DateTimeFormatter mascara = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         vendedor = (Vendedor) showInputDialog(null, "Selecione uam opção", "*** MENU VENDA ***", INFORMATION_MESSAGE, null, lista.toArray(), lista.get(0));
-        new VendedorDAO().inserir(vendedor);
+
+        Double total = parseDouble(showInputDialog("Total das vendas"));
+        String data = (showInputDialog("Data de venda (dd/mm/aaaa)"));
+        Venda venda = new Venda();
+        venda.setTotal(total);
+        venda.setVendedor(vendedor);
+        venda.setData(LocalDate.parse(data, mascara));
+        new VendaDAO().inserir(venda);
 
     }
 }
